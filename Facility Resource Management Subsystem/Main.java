798bfc2280f680f90e5ddc0ef0f8770027e09c3e
Viral Patel <152069311+virrral15@users.noreply.github.com>
2025-05-26 09:16:30 +0930

    Main.java

--- Facility Resource Management Subsystem/Main.java
+++ Facility Resource Management Subsystem/Main.java
@@ -1,25 +1,31 @@
-package FRMS;
+package facility_resource_management;
 
 public class Main {
     public static void main(String[] args) {
         CommandInvoker invoker = new CommandInvoker();
+
+        // Shared instance of RosterManager
         RosterManager rosterManager = new RosterManager();
 
-        // Register available commands
+        // ----------------- Room Management Commands -----------------
         invoker.registerCommand(1, new AllocateRoomCommand());
         invoker.registerCommand(2, new ReleaseRoomCommand());
         invoker.registerCommand(3, new DisplayRoomStatusCommand());
-        invoker.registerCommand(4, new AllocateEquipmentCommand());
-        invoker.registerCommand(5, new ReportFaultyEquipmentCommand());
-        invoker.registerCommand(6, new DisplayEquipmentStatusCommand());
-        
-        // Roster management commands
-        invoker.registerCommand(7, new CreateRosterCommand(rosterManager));
-        invoker.registerCommand(8, new AddScheduleToRosterCommand(rosterManager));
-        invoker.registerCommand(9, new UpdateShiftCommand(rosterManager));
-        invoker.registerCommand(10, new DisplayRosterCommand(rosterManager));
+        invoker.registerCommand(4, new MarkRoomUnderMaintenanceCommand());
+        invoker.registerCommand(5, new MarkRoomAvailableCommand());
 
-        // Run the menu
+        // ----------------- Equipment Management Commands -----------------
+        invoker.registerCommand(6, new AllocateEquipmentCommand());
+        invoker.registerCommand(7, new ReportFaultyEquipmentCommand());
+        invoker.registerCommand(8, new DisplayEquipmentStatusCommand());
+
+        // ----------------- Roster Management Commands -----------------
+        invoker.registerCommand(9, new CreateRosterCommand(rosterManager));
+        invoker.registerCommand(11, new AddScheduleToRosterCommand(rosterManager));
+        invoker.registerCommand(12, new UpdateShiftCommand(rosterManager));
+        invoker.registerCommand(13, new DisplayRosterCommand(rosterManager));
+
+        // ----------------- Start Program -----------------
         invoker.run();
     }
 }
